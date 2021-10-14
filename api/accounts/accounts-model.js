3ac5f3dc7962853e8ed6fc7a514e827a795f82ab
John-Blackburn <JRBbusinesscontact@gmail.com>
2021-10-13 21:25:36 -0400

    these codegrade tests are confusing me, but MVP complete. Everything works in postman

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,27 +1,38 @@
+const db = require("../../data/db-config");
+
 const getAll = () => {
-  // DO YOUR MAGIC
-}
+	return db("accounts");
+	// DO YOUR MAGIC
+};
 
-const getById = id => {
-  // DO YOUR MAGIC
-}
+const getById = (id) => {
+	return db("accounts").where("id", id).first();
+	// DO YOUR MAGIC
+};
 
-const create = account => {
-  // DO YOUR MAGIC
-}
+const create = async ({ name, budget }) => {
+	const [id] = await db("accounts").insert({ name, budget });
+	return getById(id);
+	// DO YOUR MAGIC
+};
 
-const updateById = (id, account) => {
-  // DO YOUR MAGIC
-}
+const updateById = async (id, { name, budget }) => {
+	await db("accounts").where("id", id).update({ name, budget });
+	return getById(id);
+	// DO YOUR MAGIC
+};
 
-const deleteById = id => {
-  // DO YOUR MAGIC
-}
+const deleteById = async (id) => {
+	const deletedAccount = await getById(id);
+	await db("accounts").where("id", id).delete();
+	return deletedAccount;
+	// DO YOUR MAGIC
+};
 
 module.exports = {
-  getAll,
-  getById,
-  create,
-  updateById,
-  deleteById,
-}
+	getAll,
+	getById,
+	create,
+	updateById,
+	deleteById,
+};
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,72 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const Accounts = require("./accounts-model");
+const mw = require("./accounts-middleware");
 
-router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get("/", async (req, res, next) => {
+	try {
+		const data = await Accounts.getAll();
+		res.json(data);
+	} catch (err) {
+		next(err);
+	}
+	// DO YOUR MAGIC
 });
 
-router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+router.get("/:id", mw.checkAccountId, async (req, res, next) => {
+	const id = req.params.id;
+	try {
+		const data = await Accounts.getById(id);
+		res.json(data);
+	} catch (err) {
+		next(err);
+	}
+	// DO YOUR MAGIC
+});
 
-router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
-})
+router.post(
+	"/",
+	mw.checkAccountPayload,
+	mw.checkAccountNameUnique,
+	async (req, res, next) => {
+		try {
+			const data = await Accounts.create(req.body);
+			res.json(data);
+		} catch (err) {
+			next(err);
+		}
+		// DO YOUR MAGIC
+	}
+);
+
+router.put("/:id", async (req, res, next) => {
+	const id = req.params.id;
+	try {
+		const data = await Accounts.updateById(id, req.body);
+		res.json(data);
+	} catch (err) {
+		next(err);
+	}
+	// DO YOUR MAGIC
+});
+
+router.delete("/:id", async (req, res, next) => {
+	const id = req.params.id;
+	try {
+		const data = await Accounts.deleteById(id);
+		res.json(data);
+	} catch (err) {
+		next(err);
+	}
+	// DO YOUR MAGIC
+});
+
+router.use((err, req, res) => {
+	// eslint-disable-line
+	res.status(err.status || 500).json({
+		message: err.message,
+		stack: err.stack,
+	});
+	// DO YOUR MAGIC
+});
 
 module.exports = router;
